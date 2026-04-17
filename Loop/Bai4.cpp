07335a358e718bc189aa65f1f494177a52483c2c
antlerjely96 <antlerjely96@gmail.com>
2026-04-17 18:44:32 +0700

    Loop Bai4

--- Loop/Bai4.cpp
+++ Loop/Bai4.cpp
@@ -2,3 +2,22 @@
 	4. Nhap so nguyen n tu ban phim.
 	Tinh n!
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Khai bao giaiThua
+	int giaiThua = 1;
+	//Khai bao va nhap n
+	int n;
+	cout << "n = ";
+	cin >> n;
+	//Vong lap tu 1 den n
+	for(int i = 1; i <= n; i++){
+		giaiThua *= i;
+	}
+	//In ra ket qua
+	cout << "n! = " << giaiThua << endl;
+	return 0;
+}
