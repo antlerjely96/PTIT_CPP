f8b262f052ee276d5e0d37cb0d15709bb0ab62dd
antlerjely96 <antlerjely96@gmail.com>
2026-04-17 18:51:07 +0700

    Loop Bai5

--- Loop/Bai5.cpp
+++ Loop/Bai5.cpp
@@ -2,3 +2,22 @@
 	5. Nhap so nguyen n tu ban phim.
 	Tinh tong cac so trong khoang tu 1 den n
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Khai bao tong
+	int tong = 0;
+	//Khai bao va nhap n
+	int n;
+	cout << "n = ";
+	cin >> n;
+	//Vong lap chay tu 1 den n
+	for(int i = 1; i <= n; i++){
+		tong += i;
+	}
+	//In ra ket qua
+	cout << "Tong cac so tu 1 den n: " << tong << endl;
+	return 0;
+}
