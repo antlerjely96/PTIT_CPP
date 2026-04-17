29228d03383d994b4c75cc39bc6d5c661b3dd97f
antlerjely96 <antlerjely96@gmail.com>
2026-04-17 19:32:18 +0700

    Loop Bai7

--- Loop/Bai7.cpp
+++ Loop/Bai7.cpp
@@ -2,3 +2,34 @@
 	7. Nhap so nguyen n tu ban phim
 	Kiem tra n co phai so nguyen to hay khong
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Khai bao va nhap n
+	int n;
+	cout << "n = ";
+	cin >> n;
+	//Kiem tra n
+	if(n < 2){
+		//Hien thi
+		cout << n << " khong la so nguyen to!" << endl;
+	} else {
+		int count = 0;
+		//Vong lap chay tu 2 den n / 2
+		for(int i = 2; i <= n / 2; i++){
+			//Kiem tra n % i == 0
+			if(n % i == 0){
+				count++;
+			}
+		}
+		//Kiem tra count == 0
+		if(count == 0){
+			cout << n << " la so nguyen to!" << endl;
+		} else {
+			cout << n << " khong la so nguyen to!" << endl;
+		}
+	}
+	return 0;
+}
