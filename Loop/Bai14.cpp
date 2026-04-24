ddbcb72c4f31cbbe8c1617adb1b1045609a5a643
antlerjely96 <antlerjely96@gmail.com>
2026-04-24 19:11:24 +0700

    Loop Bai14

--- Loop/Bai14.cpp
+++ Loop/Bai14.cpp
@@ -1,3 +1,18 @@
 /*
 	14. In bang cuu chuong
-*/ 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Vong lap chay i = 2 den 9
+	for(int i = 2; i <= 9; i++){
+		//Vong lap chay j = 1 den p
+		for(int j = 1; j <= 9; j++){
+			cout << i << " x " << j << " = " << i * j << "\t";
+		}
+		cout << endl;
+	}
+	return 0;
+}
