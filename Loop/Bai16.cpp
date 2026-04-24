b9d032a2294caeb07951b739aaa09eaadcd01207
antlerjely96 <antlerjely96@gmail.com>
2026-04-24 19:39:18 +0700

    Loop Bai16

--- Loop/Bai16.cpp
+++ Loop/Bai16.cpp
@@ -2,3 +2,29 @@
 	16. Nhap 2 so nguyen tu ban phim.
 	Tim uoc chung lon nhat va boi chung nho nhat cua 2 so
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Nhap 2 so
+	int a, b, c, d;
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	c = a; d = b;
+	//Kiem tra a > 0, b > 0
+	if(a > 0 && b > 0){
+		/* Tim UCLN */
+		while (d != 0){
+			int temp = d;
+			d = c % d;
+			c = temp;
+		}
+		cout << c << endl;
+		/* Tim BCNN */
+		cout << a * b / c << endl;
+	}
+	return 0;
+}
