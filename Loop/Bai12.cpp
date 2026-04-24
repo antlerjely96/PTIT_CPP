e9fb32df564b71378a84cc663fc6a5d21df8a636
antlerjely96 <antlerjely96@gmail.com>
2026-04-24 18:59:27 +0700

    Loop Bai12

--- Loop/Bai12.cpp
+++ Loop/Bai12.cpp
@@ -11,3 +11,28 @@
 	* *
 	*
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Vong lap chay i = 1 den 5
+	for(int i = 1; i <= 5; i++){
+		//Vong lap chay j = 1 den i
+		for(int j = 1; j <= i; j++){
+			cout << "* ";
+		}
+		//Xuong dong
+		cout << endl;
+	}
+	//Vong lap chay i = 1 den 5
+	for(int i = 1; i <= 5; i++){
+		//Vong lap chay j = 1 den 5 - i + 1
+		for(int j = 1; j <= 5 - i + 1; j++){
+			cout << "* ";
+		}
+		//Xuong dong
+		cout << endl;
+	}
+	return 0;
+}
